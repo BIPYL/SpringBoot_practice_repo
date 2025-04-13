3db3684ba76814f4dea08bca12532b81a2f51937
BIPYL <pruito36@gmail.com>
2025-04-13 17:29:27 +0900

    도서 리뷰 및 평점 작성/삭제 기능&평균 평점 출력 추가

--- springstarter/src/main/java/org/fastcampus/springstarter/config/InitDataConfig.java
+++ springstarter/src/main/java/org/fastcampus/springstarter/config/InitDataConfig.java
@@ -1,12 +1,15 @@
 package org.fastcampus.springstarter.config;
 
 import org.fastcampus.springstarter.entity.Book;
+import org.fastcampus.springstarter.entity.Review;
 import org.fastcampus.springstarter.service.BookService;
+import org.fastcampus.springstarter.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -15,6 +18,8 @@ public class InitDataConfig implements CommandLineRunner{
 
     @Autowired
     private BookService bookService;
+    @Autowired
+    private ReviewService reviewService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -26,7 +31,7 @@ public class InitDataConfig implements CommandLineRunner{
             book1.setPrice(30000);
             book1.setAuthor("김길동");
             book1.setPage(600);
-            bookService.save(book1);
+            bookService.save(book1); //insert sql
 
             // 여기 수정 필요!
             Book book2 = new Book(); // ← 객체 생성 빠졌었음
@@ -34,7 +39,32 @@ public class InitDataConfig implements CommandLineRunner{
             book2.setPrice(37000);
             book2.setAuthor("나길동");
             book2.setPage(800);
-            bookService.save(book2);
+            bookService.save(book2); //insert sql
+
+            //임의 리뷰 3개 생성
+            Review review01 = new Review();
+            review01.setContent("그림으로 이해하기 쉽게 해주세요");
+            review01.setRating(4);
+            review01.setCreatedAt(LocalDateTime.now());
+            review01.setBook(book1); //책정보 입력
+            reviewService.save(review01);
+
+            Review review02 = new Review();
+            review02.setContent("쉽게 설명해줘요.");
+            review02.setRating(5);
+            review02.setCreatedAt(LocalDateTime.now());
+            review02.setBook(book2);
+            reviewService.save(review02);
+
+            Review review03 = new Review();
+            review03.setContent("어려워요");
+            review03.setRating(5);
+            review03.setCreatedAt(LocalDateTime.now());
+            review03.setBook(book2);
+            reviewService.save(review03);
+
+
+
 
             }
         }
